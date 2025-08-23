bec454055a19520278af086fa4fcc0ea78e24fbb
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2025-08-23 21:33:49 +0200

    Rinominati i parametri di configurazione per il servizio Gateway

--- app/core/config.py
+++ app/core/config.py
@@ -2,20 +2,20 @@ from pydantic_settings import SettingsConfigDict, BaseSettings
 
 
 class Settings(BaseSettings):
-    SERVICE_NAME: str = "FastAPI Gateway"
-    SERVICE_VERSION: str = "0.1.0"
-    DATABASE_URL: str = "sqlite:///./database.db"
-    RABBITMQ_URL: str = "amqp://guest:guest@localhost:5672/"
-    SERVICE_PORT: int = 8000
-    USERS_SERVICE_URL: str = "http://users:8000"
-    ENVIRONMENT: str = "development"
-    SENTRY_DSN: str = ""
-    SENTRY_RELEASE: str = "0.1.0"
+    GATEWAY_SERVICE_NAME: str = "FastAPI Gateway"
+    GATEWAY_SERVICE_VERSION: str = "0.1.0"
+    GATEWAY_DATABASE_URL: str = "sqlite:///./database.db"
+    GATEWAY_RABBITMQ_URL: str = "amqp://guest:guest@localhost:5672/"
+    GATEWAY_SERVICE_PORT: int = 8000
+    GATEWAY_USERS_SERVICE_URL: str = "http://users:8000"
+    GATEWAY_ENVIRONMENT: str = "development"
+    GATEWAY_SENTRY_DSN: str = ""
+    GATEWAY_SENTRY_RELEASE: str = "0.1.0"
 
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    REFRESH_TOKEN_EXPIRE_DAYS: int = 30
-    PRIVATE_KEY: str = "./certs/private.pem"
-    PUBLIC_KEY: str = "./certs/public.pem"
+    GATEWAY_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    GATEWAY_REFRESH_TOKEN_EXPIRE_DAYS: int = 30
+    GATEWAY_PRIVATE_KEY: str = "./certs/private.pem"
+    GATEWAY_PUBLIC_KEY: str = "./certs/public.pem"
 
     model_config = SettingsConfigDict(env_file=".env")
 
